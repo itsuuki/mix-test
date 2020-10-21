a62883585796eeb506f13354c5284cf399256f2a
itsuuki <53384302+itsuuki@users.noreply.github.com>
2020-10-21 09:24:19 +0900

    Readme-commits

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\User;
+use App\Models\Address;
 
 class UserController extends Controller
 {
     public function show($id)
     {
         $user = User::findOrFail($id);
+        $address = Address::where('item_id', $id)->get();
         // echo var_dump($image);
         return view('user.show', ['user' => $user]);
     }
